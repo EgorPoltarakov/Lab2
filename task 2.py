ad01c0a8fddfcda8b3a049e62fc7e0974d2a59f5
Poltarakov <epoltarakov.99@mail.ru>
2023-01-25 09:05:00 +0300

    task 2

---
+++ task 2.py
@@ -0,0 +1,16 @@
+def func():
+    value = int(input("введите число "))
+    maxValue = value
+
+    if value == 0:
+        return maxValue
+
+    nextValue = func()
+
+    if maxValue < nextValue:
+        maxValue = nextValue
+
+    return maxValue
+
+
+print(func())
